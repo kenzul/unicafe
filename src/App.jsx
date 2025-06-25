9cda81169662d6b570b11181fe6bfc1cbdfa41c7
Jean Alvarez <blackraisondetre126@gmail.com>
2025-06-25 16:40:24 -0500

    Create Feedback component

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,17 @@ const Counter = ({ name, count }) => {
   return <p>{name} {count}</p>
 }
 
+const Feedback = ({ handleGoodClick, handleNeutralClick, handleBadClick }) => {
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <Button onClick={handleGoodClick} text="Good"></Button>
+      <Button onClick={handleNeutralClick} text="Neutral"></Button>
+      <Button onClick={handleBadClick} text="Bad"></Button>
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -19,11 +30,14 @@ const App = () => {
 
   return (
     <div>
-      <Button onClick={createButtonHandler(setGood, good)} text="Good"></Button>
-      <Button onClick={createButtonHandler(setNeutral, neutral)} text="Neutral"></Button>
-      <Button onClick={createButtonHandler(setBad, bad)} text="Bad"></Button>
+      <Feedback
+        handleGoodClick={createButtonHandler(setGood, good)}
+        handleBadClick={createButtonHandler(setBad, bad)}
+        handleNeutralClick={createButtonHandler(setNeutral, neutral)}
+      ></Feedback>
     </div>
   )
 }
+
 
 export default App;
